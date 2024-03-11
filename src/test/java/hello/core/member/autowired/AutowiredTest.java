cf98a1d9dffbf69567f1c16c293e62e592794fa7
gomhyeok <gomhyeok@gmail.com>
2024-03-11 18:17:42 +0900

    스프링 Provider - 스프링의 싱글톤과 prototype 빈에 대한 학습내요 - 자바 표준을 사용하거나 spring에서 제공하는 objectProvider를 사용하거나 둘중 한가지

--- src/test/java/hello/core/member/autowired/AutowiredTest.java
+++ src/test/java/hello/core/member/autowired/AutowiredTest.java
@@ -1,4 +1,4 @@
-package hello.core.autowired;
+package hello.core.member.autowired;
 
 import hello.core.member.Member;
 import io.micrometer.common.lang.Nullable;
---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,38 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonTest {
+
+    @Test
+    void singleToneBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);//클래스 직접 삽입시 자동으로 ComponentScan
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+        assertThat(singletonBean1).isSameAs(singletonBean2);
+
+        ac.close();
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("singletonBean.inti");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("singletonBean.destroy");
+        }
+    }
+}
